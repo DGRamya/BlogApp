ad31be5c8153f6d10240c19d397fc894cc7dcbe6
rdasego <rdasego@iu.edu>
2018-01-04 03:07:09 -0500

    APIs for creating blogs and diaplaying them

--- public/app.js
+++ public/app.js
@@ -9,10 +9,24 @@
     function BlogController($scope,$http){
         $scope.createPost = createPost;
 
+        function init(){
+            getAllPosts();
+        }
+        init();
+        function getAllPosts(){
+            $http
+                .get("/api/blogpost")
+                .success(function(posts){
+                    $scope.posts = posts;
+                })
+        }
+
         function createPost(post){
-            console.log("createPost!!!");
+
             console.log(post);
-            $http.post("/api/blogpost",post);
+            $http
+                .post("/api/blogpost",post)
+                .success(getAllPosts);
         }
     }
 })();
--- server.js
+++ server.js
@@ -5,17 +5,62 @@
 var express = require('express');
 var app = express();
 
-    // var bodyParser = require('body-parser');
-    //
-    // app.use(bodyParser.json());
-    // app.use(bodyParser.urlencoded({ extended: true }));
-    // // configure a public directory to host static content
+    var bodyParser = require('body-parser');
+
+     app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    //connect to mongoDB
+    var mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost/myBlogApp');
+
+    //create schema to store the data
+    var postSchema = mongoose.Schema({
+        title : {type: String, required: true},
+        body: String,
+        tag:{type:String,enum:['POLITICS','ECONOMY','EDUCATION']},
+        posted: {type: Date, default: Date.now()}
+
+    },{collection:'post'});
+
+    var PostModel = mongoose.model("PostModel",postSchema);
+
+
+
+    // configure a public directory to host static content
     app.use(express.static(__dirname + '/public'));
     app.post("/api/blogpost",createPost);
+    app.get("/api/blogpost",getAllPosts);
+
+    function getAllPosts(req,res){
+        PostModel
+            .find()
+            .then(
+                function(posts){
+                    res.json(posts);
+                },
+                function(err){
+                    res.sendStatus(400);
+                }
+            );
+    }
 
     function createPost(req,res){
+        var post= req.body;
         console.log("Hello from server ");
-       // res.send(200);
+       console.log(post);
+       PostModel
+           .create(post)
+           .then(
+               function(postObj){
+                   res.json(200);
+               },
+                function(){
+                    res.sendStatus(400);
+                }
+
+        );
+
     }
 
     //require("./server/app.js")(app);
